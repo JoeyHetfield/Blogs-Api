e0b14ae454483f89808a4d28c1b42f726ee971fb
Joey Hetfield <joeydefenix@hotmail.com>
2023-04-06 15:47:42 -0300

    Migration de blog_posts

---
+++ src/migrations/20230406183439-create-blog_posts.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        }
+      },     
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },     
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },     
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  },
+};
